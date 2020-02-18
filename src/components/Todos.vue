3fce51e3ee3bf43eb608715a7323616da95406f6
Figeac Jean-Armand <Jean-Armand.Figeac@swisscom.com>
2020-02-18 16:07:51 +0100

    specific message

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <div :key="todo.id" v-for="todo in todos">
+        <TodoItem :todo="todo"/>
+    </div>    
+</div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name:"Todos",
+    components: {
+        TodoItem
+    },
+   props: [
+       todos
+    ]
+}
+</script>
+
+<style scoped>
+
+
+</style>
